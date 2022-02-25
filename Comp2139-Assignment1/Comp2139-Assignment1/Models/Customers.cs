770c52c2e2d33c55b98df901aff272c8ce1b7fe8
janine usana <janinem.usana@gmail.com>
2022-02-25 01:40:55 -0500

    reduce redundancy done for customer, prod, tech forms

--- Comp2139-Assignment1/Comp2139-Assignment1/Models/Customers.cs
+++ Comp2139-Assignment1/Comp2139-Assignment1/Models/Customers.cs
@@ -8,25 +8,33 @@ namespace Comp2139_Assignment1.Models
         public int CustomerId { get; set; }
 
         [Required(ErrorMessage = "Customer needs to have First Name" )]
+        [Display(Name ="First Name")]
         public string? CustomerFirstName { get; set; }
 
         [Required(ErrorMessage = "Customer needs to have Last Name")]
+        [Display(Name = "Last Name")]
         public string? CustomerLastName { get; set; }
 
         [Required(ErrorMessage = "Customer needs to have an Address")]
+        [Display(Name = "Address")]
         public string? CustomerAddress { get; set; }
 
-        [Required(ErrorMessage = "Customer needs to have a city")]
+        [Required(ErrorMessage = "Customer needs to have a City")]
+        [Display(Name = "City")]
         public string? CustomerCity { get; set; }
 
         [Required(ErrorMessage = "Customer needs to have a State")]
+        [Display(Name = "State")]
         public string? CustomerState { get; set; }
 
         [Required(ErrorMessage = "Customer needs to have a Country")]
+        [Display(Name = "Country")]
         public string? CustomerCountry { get; set; }
 
+        [Display(Name = "Email")]
         public string? CustomerEmail { get; set; }
 
+        [Display(Name = "Phone")]
         public string? CustomerPhone { get; set; }
         
     }
